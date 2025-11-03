f40243e01e74e73aae22dd7d2b6bac77788411e0
Lakshya Gupta <lakshyagupta916640@gmail.com>
2025-11-03 14:31:27 +0530

    add disabled prop to the button

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -7,6 +7,7 @@ interface ButtonProps {
   onClick?: () => void;
   customCSS?: string;
   isActive?: boolean;
+  disabled?: boolean;
 }
 
 const varientColor = {
@@ -22,6 +23,7 @@ export function Button({
   onClick,
   isActive,
   customCSS,
+  disabled,
 }: ButtonProps) {
   let startIconStyle: string;
   if (startIcon) startIconStyle = "mr-3";
@@ -30,9 +32,12 @@ export function Button({
   return (
     <button
       onClick={onClick}
+      disabled={disabled}
       className={` ${isActive ? activeCSS : ""} ${customCSS} ${
         varientColor[varient]
-      }  font-normal rounded-lg px-3 py-2 hover:cursor-pointer hover:shadow-xl/3 hover:shadow-back-800`}>
+      }  font-normal rounded-lg px-3 py-2 hover:cursor-pointer hover:shadow-xl/3 hover:shadow-back-800
+      ${disabled ? "opacity-50 cursor-not-allowed" : ""} 
+      `}>
       <div className="flex items-center">
         <div className={startIconStyle}>{startIcon}</div>
         {text}
